eca9d7cd819ff0c83ecf10f4af6b74f27ecbf327
tris43210 <tristan.g.king@gmail.com>
2025-11-07 03:18:00 +0000

    cleaning up code

--- client/src/main.js
+++ client/src/main.js
@@ -21,7 +21,9 @@ renderer.setPixelRatio(window.devicePixelRatio);
 // Sets the viewport to the monitors resolution
 renderer.setSize( window.innerWidth, window.innerHeight);
 //How zoomed in we are to the object
-camera.position.setZ(60);
+camera.position.setZ(10);
+camera.position.setY(10); 
+camera.position.setX(20); 
 
 renderer.render( scene, camera );
 
@@ -29,7 +31,7 @@ renderer.render( scene, camera );
 // 	radius, tubeRadius,
 // 	radialSegments, tubularSegments )
 
-const geometry = new THREE.TorusGeometry( 16, 5, 30, 100)
+const geometry = new THREE.TorusGeometry( 15, 1, 30, 100)
 
 // Basic material not affected by light source
 
@@ -44,10 +46,29 @@ const torus = new THREE.Mesh (geometry, material);
 
 scene.add(torus)
 
+
+
+// const gltfLoad = new GLTFLoader();
+// gltfLoad.load('./public/nike_air_max_akatsuki/scene.gltf', function(nikeModel) {
+//     console.dir(nikeModel)
+//     nikeModel.scene.scale.x = 10
+//     nikeModel.scene.scale.y = 10
+//     nikeModel.scene.scale.z = 10
+//     scene.add(nikeModel.scene)
+//     return nikeModel
+// })
+
+
+let nikeModel;   // <-- Global reference
+
 const gltfLoad = new GLTFLoader();
-gltfLoad.load('./public/nike_air_max_akatsuki/scene.gltf', function(nikeModel) {
-    scene.add(nikeModel.scene)
-})
+gltfLoad.load('./public/nike_air_max_akatsuki/scene.gltf', (gltf) => {
+    nikeModel = gltf.scene;     // <-- assign to global
+    nikeModel.scale.set(10, 10, 10);
+    nikeModel.position.set(0, -4, 0);
+    scene.add(nikeModel);
+});
+
 
 
 // Directional lighting
@@ -61,63 +82,70 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 
 const gridHelper = new THREE.GridHelper(200,50);
 
+scene.add(pointLight)
 
-scene.add(pointLight, lightHelper, gridHelper)
 
 
 // Flat Lighting
 const color = 0xffffff;
-const intensity = 10;
+const intensity = 20;
 const light = new THREE.AmbientLight(color, intensity);
 scene.add(light);
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Render/Animation Loop Below 
-
-// request animation frame is similar to setInterval except that when you navigate away 
-// from the page requestAnimationFrame pauses the animation
 
 function addStar() {
-    const geometry = new THREE.SphereGeometry(.25,24,24); 
-    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const geometry = new THREE.SphereGeometry(.10,24,24); 
+    const material = new THREE.MeshStandardMaterial({color: 0x3B3B3B});
     const star = new THREE.Mesh(geometry,material)
 
     const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 200 )); 
 
     star.position.set(x, y, z); 
     scene.add(star)
-
 }
 
-Array(600).fill().forEach(addStar); 
+Array(1000).fill().forEach(addStar); 
 
-const spaceTexture = new THREE.TextureLoader().load('public/0203049~medium.jpg');
-scene.background = spaceTexture;
+// const spaceTexture = new THREE.TextureLoader().load('public/0203049~medium.jpg');
+// scene.background = spaceTexture;
+
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    nikeModel.rotation.x += 0.05;
+    nikeModel.rotation.y += 0.05;
+    nikeModel.rotation.z += 0.05;
+
+    camera.position.z = t * -0.01
+    camera.position.y = t * -0.01
+    camera.position.x = t * -0.01
+}
+
+document.body.onscroll = moveCamera
+
+// Render/Animation Loop Below 
+
+// request animation frame is similar to setInterval except that when you navigate away 
+// from the page requestAnimationFrame pauses the animation
 
 function animate() {
     requestAnimationFrame(animate)
 
-    torus.rotation.x += 0.001;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
+    // torus.rotation.x -= 0.0005;
+    torus.rotation.y -= 0.0005;
+    // torus.rotation.z -= 0.0001;
+
+    // nikeModel.rotation.x += 0.0009;
+    nikeModel.rotation.y += 0.0009;
+    // nikeModel.rotation.z += 0.0009;
+
+   
 
     renderer.render( scene, camera);
 }
 animate()
 
-function moveCamera() {
-        const t = document.body.getBoundingClientRect().top;
-            nikeModel.rotation.x += 0.05;
-                nikeModel.rotation.y += 0.05;
-                    nikeModel.rotation.z += 0.05;
 
-                        camera.position.z = t * -0.01
-                            camera.position.y = t * -0.01
-                                camera.position.x = t * -0.01
-
-
-                                }
-                                document.body.onscroll = moveCamera
-}
